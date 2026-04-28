9a9a72ef4e7a50f8b027df1909dd23d8f0db8c18
Sagar <your-email@gmail.com>
2026-04-28 18:28:33 +0530

    updated UI

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <h1> DevOps React App</h1>
        <h2>Version 2 - Updated,  hosted then made changes with the app then deployed again with those chnges cicd integrated and changes triggered, another change 12AM .</h2>
-      <p>Deployed using Kubernetes on GCP, changes made at 5:30 by creating branch and then pusing it into main</p>
+      <p>Deployed using Kubernetes on GCP, changes made at 6:30 by creating branch and then pusing it into main</p>
       
       <p>Status: Running Successfully </p>
     </div>
